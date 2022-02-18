1f896da80818709e02f209c218d5294ccbf3f457
Jeppa <jeppa@geocities.com>
2022-02-18 18:33:24 +0100

    Small file cleanup

--- core/src/main/java/com/ericdebouwer/petdragon/listeners/EggListener.java
+++ core/src/main/java/com/ericdebouwer/petdragon/listeners/EggListener.java
@@ -34,50 +34,49 @@ public class EggListener implements Listener {
 			return;
 		}
 
-		//New version of egg place: event ALWAYS is canceled, check is delayed, egg gets removed from hand an given back if no spawn is possible!
-		ItemStack dragEgg=e.getItemInHand().clone();			//save the egg
-		dragEgg.setAmount(1);									//only ONE egg...
-		fixItemInHand(e);										//remove the egg
-		e.setCancelled(true);									//stop the event
+		//Jeppa: new version of egg place: event is ALWAYS canceled, check is delayed, egg gets removed from hand an given back if no spawn is possible!
+		ItemStack dragEgg=e.getItemInHand().clone(); 			//save the egg
+		dragEgg.setAmount(1); 									//only ONE egg...
+		fixItemInHand(e); 										//remove the egg
+		e.setCancelled(true); 									//stop the event
 		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
-			e.getBlock().setType(Material.DRAGON_EGG,false);	//place an egg (fake the event result)		//may be removed again.. block is protectet by interact event...for 0.5 seconds..
+			e.getBlock().setType(Material.DRAGON_EGG,false); 	//place an egg (fake the event result), block is protectet by interact event...for 0.5 seconds..
 			e.getBlock().setMetadata("isPetEgg", new FixedMetadataValue(plugin,true));//Set a marker for the 'dontTouch'-event :)
 		});
-		
-        UUID uuid = e.getPlayer().getUniqueId();
-        plugin.getDragonLocations().getLocationList(uuid); //returns a list with valid Locations for this player and activates the chunks! (list only if the chunks have been activated already!)
-        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
-        	if (plugin.getConfigManager().isEggAbidesDragonMax() && !e.getPlayer().hasPermission("petdragon.bypass.dragonlimit")) {
-//     		if (plugin.getConfigManager().useMaxDragonsForEggs && !e.getPlayer().hasPermission("petdragon.bypass.dragonlimit")){	//Jeppa's version until 1.5.3
-        		int dragonCount = plugin.getFactory().getDragons(e.getPlayer()).size();
-        		if (dragonCount >= plugin.getConfigManager().getMaxDragons()){
-        			plugin.getConfigManager().sendMessage(e.getPlayer(), Message.DRAGON_LIMIT, ImmutableMap.of("amount", String.valueOf(plugin.getConfigManager().getMaxDragons()))); //config says the given values is maximum amount! so don't use dragonCount (real amount)!!
-        			// "cancel" the event...
-        			e.getBlock().setType(Material.AIR); 		//remove the placed egg
-        			if (e.getBlock().hasMetadata("isPetEgg")) {
-        				e.getBlock().removeMetadata("isPetEgg", plugin);
-        			}
-        			giveItemInHand(e, dragEgg);					//give back the egg
-        			return;
-        		}
-        	}
 
-        	if (e.getPlayer().getGameMode() == GameMode.CREATIVE && !plugin.getConfigManager().isAlwaysUseUpEgg()){
-    			giveItemInHand(e, dragEgg);						//give back the egg
-    		}
-    		
-        	PetEnderDragon dragon = plugin.getFactory().create(e.getBlock().getLocation().add(0, 3, 0), e.getPlayer().getUniqueId());
-        	dragon.spawn();
-        	plugin.getConfigManager().sendMessage(e.getPlayer(), Message.EGG_HATCHED, null);
-    		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
-    			e.getBlock().setType(Material.AIR);
-    			if (e.getBlock().hasMetadata("isPetEgg")) {
-    				e.getBlock().removeMetadata("isPetEgg", plugin);
-    			}
-    		});
-        },10);
+		UUID uuid = e.getPlayer().getUniqueId();
+		plugin.getDragonLocations().getLocationList(uuid); //returns a list with valid Locations for this player (not used yet) and activates the chunks!
+		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+			if (plugin.getConfigManager().isEggAbidesDragonMax() && !e.getPlayer().hasPermission("petdragon.bypass.dragonlimit")) {
+				int dragonCount = plugin.getFactory().getDragons(e.getPlayer()).size();
+				if (dragonCount >= plugin.getConfigManager().getMaxDragons()){
+					plugin.getConfigManager().sendMessage(e.getPlayer(), Message.DRAGON_LIMIT, ImmutableMap.of("amount", String.valueOf(plugin.getConfigManager().getMaxDragons()))); //config says the given values is maximum amount! so don't use dragonCount (real amount)!!
+					// "cancel" the event...
+					e.getBlock().setType(Material.AIR); 		//remove the placed egg
+					if (e.getBlock().hasMetadata("isPetEgg")) {
+						e.getBlock().removeMetadata("isPetEgg", plugin);
+					}
+					giveItemInHand(e, dragEgg);					//give back the egg
+					return;
+				}
+			}
+
+			if (e.getPlayer().getGameMode() == GameMode.CREATIVE && !plugin.getConfigManager().isAlwaysUseUpEgg()){
+				giveItemInHand(e, dragEgg);						//give back the egg
+			}
+
+			PetEnderDragon dragon = plugin.getFactory().create(e.getBlock().getLocation().add(0, 3, 0), e.getPlayer().getUniqueId());
+			dragon.spawn();
+			plugin.getConfigManager().sendMessage(e.getPlayer(), Message.EGG_HATCHED, null);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+				e.getBlock().setType(Material.AIR);
+				if (e.getBlock().hasMetadata("isPetEgg")) {
+					e.getBlock().removeMetadata("isPetEgg", plugin);
+				}
+			});
+		},10);
 	}
-	
+
 	private void fixItemInHand(BlockPlaceEvent e) {
 		if (e.getItemInHand().getAmount() > 1) {
 			e.getItemInHand().setAmount(e.getItemInHand().getAmount() - 1);
@@ -97,7 +96,7 @@ public class EggListener implements Listener {
 			if (e.getPlayer().getInventory().getItem(e.getHand()).getType().equals(Material.AIR)) {
 				e.getPlayer().getInventory().setItem(e.getHand(), item);
 //				e.getItemInHand().setAmount(1);
-			} else { //Hand has Item, but not a PetEgg, drop the item, so the player can pick it up...
+			} else { //Hand has Item, but not a PetDragonEgg, drop the item, so the player can pick it up...
 				e.getPlayer().getWorld().dropItem(e.getPlayer().getLocation(), item);
 			}
 		}
@@ -105,7 +104,7 @@ public class EggListener implements Listener {
 	
 	@EventHandler
 	public void dontTouchTheEgg(PlayerInteractEvent event){
-		if (event.getClickedBlock().hasMetadata("isPetEgg")){ //Block has temporary meta -> is placed Block for hatching...
+		if (event.getClickedBlock().hasMetadata("isPetEgg")){ //Block has temporary meta -> it is placed egg for hatching...
 			event.setCancelled(true);
 		}
 	}
